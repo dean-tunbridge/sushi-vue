8a24aa35483583c92f709643ce8169afb28a0bc7
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-24 10:24:07 +1200

    begin creating navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts"></script>
 
-<template><h1>Hello</h1></template>
+<template>
+  <header>
+    <nav class="header__nav">
+      <div class="header__logo">
+        <h4>SUSHIBONSON</h4>
+        <div class="header__logo-overlay"></div>
+      </div>
+
+      <ul class="header__menu">
+        <li>
+          <a href="#Menu">Menu</a>
+        </li>
+        <li>
+          <a href="#Food">Food</a>
+        </li>
+        <li>
+          <a href="#Servcies">Servcies</a>
+        </li>
+        <li>
+          <a href="#About-us">About Us</a>
+        </li>
+        <li>
+          <img src="/assets/search.svg" alt="search" />
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
 
 <style lang="css" scoped></style>
