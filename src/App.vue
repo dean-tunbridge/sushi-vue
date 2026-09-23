39093820891495044ba241da49505f475901dc66
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-24 10:31:43 +1200

    begin adding styling to navbar.

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,42 @@
           <img src="/assets/search.svg" alt="search" />
         </li>
       </ul>
+
+      <ul class="header__menu-mobile">
+        <img src="/assets/menu.svg" alt="menu" />
+      </ul>
     </nav>
   </header>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.header__nav {
+  display: flex;
+  overflow: hidden;
+}
+
+.header__logo {
+  flex: 1;
+  display: flex;
+  position: relative;
+  padding: 20px;
+}
+
+.header__logo-overlay {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  background-color: var(--primary-color);
+  z-index: -1;
+}
+
+.header__logo h4 {
+  padding-left: 30px;
+  font-weight: 700;
+  font-size: 24px;
+  text-transform: uppercase;
+  font-family: var(--playfair-display);
+  color: #fff;
+  opacity: 0.8;
+}
+</style>
