16014e297064ee30ff6774987dc4b72c9558b9cd
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-24 12:45:55 +1200

    troubleshooting aos

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init({
+  duration: 1000,
+  offset: 100,
+})
+
 createApp(App).mount('#app')
