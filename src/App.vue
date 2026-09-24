f1a1a610cd67b90b45fe7c4081285036081b17be
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-24 12:57:51 +1200

    complete popular component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import About from './components/pages/About.vue'
 import Header from './components/pages/Header.vue'
 import Hero from './components/pages/Hero.vue'
+import Popular from './components/pages/Popular.vue'
 </script>
 
 <template>
   <Header />
   <Hero />
   <About />
+  <Popular />
 </template>
 
 <style scoped></style>
